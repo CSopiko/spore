99ce009e40ba0a8ec0135dc6e2a6637a6ca4595e
CSopiko <skurd18@freeuni.edu.ge>
2021-11-13 07:54:58 +0400

    Update init

--- app/__init__.py
+++ app/__init__.py
@@ -2,30 +2,55 @@ from typing import Tuple
 from log import *
 from creature import Creature, Predator, Prey
 
-SIMULATION_NUM = 1
+SIMULATION_NUM = 100
 
 
 def main() -> None:
     for _ in range(SIMULATION_NUM):
+        log_start_simulation()
         predator, prey = evolve_creatures()
-        log_characteristic(predator, 'Predator')
-        log_characteristic(prey, 'Prey')
-        chase(predator, prey)
-        combat(predator, prey)
+        if chase(predator, prey):
+            combat(predator, prey)
 
 
 def evolve_creatures() -> Tuple[Creature, Creature]:
     prey = Prey()
-    predator = Predator()
+    predator = Predator(prey.position)
+    # log_characteristic(predator, 'Predator')
+    # log_characteristic(prey, 'Prey')
     return predator, prey
 
 
-def chase(predator, prey) -> None:
-    pass
+def chase(predator, prey) -> bool:
+    """
+    :param predator: Creature chases something to eat
+    :param prey: Creature that's wanted to be eaten
+    :return: whether or not Creatures enter the combat
+    """
+    while True:
+        if predator.get_position() >= prey.get_position():
+            return True
+        if predator.get_stamina() <= 0:
+            log_predator_runs_out_of_stamina()
+            return False
+        predator.movement()
+        prey.movement()
+        # log_characteristic(predator, 'Predator')
+        # log_characteristic(prey, 'Prey')
 
 
-def combat(predator, prey):
-    pass
+def combat(predator, prey) -> None:
+    log_combat_starts()
+    log_characteristic()
+    while True:
+        if predator.get_health() <= 0:
+            log_predator_runs_out_of_health()
+            return
+        if prey.get_health() <= 0:
+            log_prey_runs_out_of_health()
+            return
+        prey.health += predator.attack()
+        predator.health += prey.attack()
 
 
 if __name__ == '__main__':
